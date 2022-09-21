4fd13ed2ab21f0bc02b8c0d40b17720d03d4a7af
Maria Rios <kanelapr@gmail.com>
2022-09-20 20:15:47 -0400

    day 6 activity 1

--- models/band.js
+++ models/band.js
@@ -9,19 +9,41 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({MeetGreet}) {
+      // meet and greets
+      Band.hasMany(MeetGreet, {
+          foreignKey: "band_id",
+          as: "meet_greets"
+      })
     }
   }
   Band.init({
-    band_id: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    genre: DataTypes.TEXT,
-    available_start_time: DataTypes.DATE,
-    end_time: DataTypes.DATE
+    band_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    available_start_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    end_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
   }, {
     sequelize,
     modelName: 'Band',
+    tableName: 'bands',
+    timestamps: false
   });
   return Band;
 };
